7c319a4ce75c1bda9010cb075065c1f2d2b1ebaf
Emre Cigit <125315547+emrecigit@users.noreply.github.com>
2023-03-28 00:00:14 +0300

    2303280000

--- src/gitGub/gitHub_ilkDers.java
+++ src/gitGub/gitHub_ilkDers.java
@@ -1,7 +1,10 @@
 package gitGub;
 
+import java.util.Scanner;
+
 public class gitHub_ilkDers {
     public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
    /*Bilgisayrda cmd komut işlemleri
    Bilgisayarda git varsa versionu kontrol etme :git --verison
    Terminalde kullanılan konutları renklendirir :git config --global color.ui true
@@ -12,9 +15,10 @@ public class gitHub_ilkDers {
    local  :sadecd o an kullanılan repo icin tanımlar.
     */
     /*
-    Bilgisayarda git i initalize edip tanıştırma yapılır 1 kez yapılır:git init
-
-
+    Bilgisayarda git i initalize edip tanıştırma yapılır bir git dosyası olusturur 1 kez yapılır:git init
+    Repository e oluşan dosyayı ve yeni değişiklikleri eklemek icin (. hersey anlamına gelir ):git add .
+    Terminal ekranını temizlemek için:clear
+    eklenen dosyaı local repository'e version olarak yüklemek icin:git commit -m"2303272357"
 
      */
     }
