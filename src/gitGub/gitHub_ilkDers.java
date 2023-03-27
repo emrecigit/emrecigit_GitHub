04f0d420d4c5e2d227ee37b165e91d7ab3b0d1db
Emre Cigit <125315547+emrecigit@users.noreply.github.com>
2023-03-28 00:15:46 +0300

    2303280015

--- src/gitGub/gitHub_ilkDers.java
+++ src/gitGub/gitHub_ilkDers.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class gitHub_ilkDers {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-   /*Bilgisayrda cmd komut işlemleri
+   /*
+   Bilgisayrda cmd komut işlemleri
    Bilgisayarda git varsa versionu kontrol etme :git --verison
    Terminalde kullanılan konutları renklendirir :git config --global color.ui true
    Yapılan islemlerde görünen ismi degistirir:git config --global user.name"emre.cigit"
@@ -14,12 +15,23 @@ public class gitHub_ilkDers {
    system :tum kullanıcılar ve tum repolar icin
    local  :sadecd o an kullanılan repo icin tanımlar.
     */
+    //======================================================================
     /*
-    Bilgisayarda git i initalize edip tanıştırma yapılır bir git dosyası olusturur 1 kez yapılır:git init
-    Repository e oluşan dosyayı ve yeni değişiklikleri eklemek icin (. hersey anlamına gelir ):git add .
-    Terminal ekranını temizlemek için:clear
-    eklenen dosyaı local repository'e version olarak yüklemek icin:git commit -m"2303272357"
+    1-Terminal ekranını temizlemek için:clear
+    İlk Kurulum sırası
+    2-Bilgisayarda git i initalize edip tanıştırma yapılır bir git dosyası olusturur 1 kez yapılır:git init
+    3-Repository e oluşan dosyayı ve yeni değişiklikleri eklemek icin (. hersey anlamına gelir ):git add .
 
+    4-Eklenen dosyaı local repository'e version olarak yüklemek icin:git commit -m"2303272357"
+    5-commit ten sonra yapılacak islem:git branch -M main
+    6-:git remote add origin https://github.com/emrecigit/emrecigit_GitHubTeam113
+    7-:git push -u origin main
+    Sonraki işlemler de 6-5-7 diye ilerler
      */
+      //======================================================================
+
+
+
+
     }
 }
