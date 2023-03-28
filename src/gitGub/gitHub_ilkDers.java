841d9b6cacb52e8e81227712bb7c1cf62ae8d5a9
Emre Cigit <125315547+emrecigit@users.noreply.github.com>
2023-03-28 21:02:42 +0300

    230282102

--- src/gitGub/gitHub_ilkDers.java
+++ src/gitGub/gitHub_ilkDers.java
@@ -23,13 +23,27 @@ public class gitHub_ilkDers {
     3-Repository e oluşan dosyayı ve yeni değişiklikleri eklemek icin (. hersey anlamına gelir ):git add .
     4-Eklenen dosyaı local repository'e version olarak yüklemek icin:git commit -m"2303272357"
     5-commit ten sonra yapılacak islem:git branch -M main
-    6-:git remote add origin https://github.com/emrecigit/emrecigit_GitHubTeam113
+    6-:git remote add origin https://github.com/emrecigit/GitHubTeam113.git (hata olursa) add yerine set-url
     7-:git push -u origin main
     Sonraki işlemler de 3-4-7 diye ilerler
+    */
+    //=================================================================================
+    /*
+    git status:durumu görür.
     error : failed to push some refs to hatası alınırsa:git push -f origin master
+    git pull https://github.com/emrecigit/GitHubTeam113.git terminalden çekmek
+    git log : git te yapılan değişiklikleri gösterir git log --oneline(tek satırda) sonuna gelmek için q ye bas
+    git checkout 012458585 log daki herahngi bir degere döner (onceki commit)
+    git revert 012458585 yazarsak bu son durum kalıcı olur sonrasında da wq yazlıp enter a basılır
      */
      //======================================================================
-
+     /*
+      branch listesi görmek icin:git branch
+      yeni branch olustyrmak icin:git branch branch ismi
+      istenen brancha geçmek icin:git checkout branch ismi
+      baska bir branch'i kullandıgımız branch ile birleştirme:git merge branchismi
+      Aynı kod satırınıda farklı kodlar varsa CONFLICT olusur manuel düzeltilebilir düzeltip add commit push yapılır
+      */
 
 
 
